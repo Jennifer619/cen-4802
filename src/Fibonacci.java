659edaf55f0a2d9785f477b232e4abad2fbdbc73
Jen <jmiller283@mail.valenciacollege.edu>
2022-01-18 19:29:15 -0500

    add javadoc

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,6 +1,13 @@
-
+/**
+ * @author jenni
+ *
+ */
 public class Fibonacci {
 	// recursive method
+	/**
+	 * @param n
+	 * @return nth integer of fibonacci sequence
+	 */
 	public static int fib (int n) { 
 		if (n <= 1) {
 			return n;
@@ -8,6 +15,11 @@ public class Fibonacci {
 		return fib(n - 1) + fib(n - 2);
 	}
 	// main method
+	
+	/**
+	 * @param args
+	 * takes n integer value and outputs it in text
+	 */
 	public static void main (String args[]) {
 		int n = 10;
 		System.out.println("The 10th integer in the Fibonacci sequence is: " + fib(n));
