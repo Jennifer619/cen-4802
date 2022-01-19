251d03e18134a02adfdad3cca45a02a515c69216
Jen <jmiller283@mail.valenciacollege.edu>
2022-01-18 19:16:24 -0500

    create class

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,15 +1,4 @@
 
 public class Fibonacci {
-	// recursive method
-	public static int fib (int n) { 
-		if (n <= 1) {
-			return n;
-		} 
-		return fib(n - 1) + fib(n - 2);
-	}
-	// main method
-	public static void main (String args[]) {
-		int n = 10;
-		System.out.println("The 10th integer in the Fibonacci sequence is: " + fib(n));
-	}
+
 }
